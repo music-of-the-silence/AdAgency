c631507b8df25a19a6a99d7592ac2400a8949fd6
Chandan Nath <144007351+music-of-the-silence@users.noreply.github.com>
2025-06-14 20:07:33 +0530

    updated files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/AdAgency/", // Match your GitHub repo name EXACTLY
+  base: "/AdAgency/", // Must match your repo name exactly
   build: {
-    outDir: "dist",
-    emptyOutDir: true,
+    outDir: "build",
+    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
 });
