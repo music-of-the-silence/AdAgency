5077e59bfbde205e5b98e7fb7ebf5012a656cb27
Chandan Nath <144007351+music-of-the-silence@users.noreply.github.com>
2025-06-14 23:37:08 +0530

    Add deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: '/AdAgency/', // Add this line - should match your repository name
-  plugins: [react()], // Add this line
+  base: '/AdAgency/',
+  plugins: [react()],
   build: {
     outDir: "build",
     assetsDir: "assets",
