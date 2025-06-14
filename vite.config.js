1e34ff21ddc22901442d80c40a51715296014863
Chandan Nath <144007351+music-of-the-silence@users.noreply.github.com>
2025-06-14 17:44:44 +0530

    New Addition

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/AdAgency/", // Match your GitHub repo name EXACTLY
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+  },
+});
